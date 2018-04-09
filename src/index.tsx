5bab511391012a9fea5ea3eb74aa9124332e84dc
airtoxin <airtoxin@icloud.com>
2018-04-10 00:48:47 +0900

    Functionize

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import * as ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { App } from "./App";
 import {store} from "./store";
-import {draw} from "./automaton/canvas";
-import {initialize, getNext} from "./automaton/Automaton";
-import * as strategies from "./automaton/strategies";
+import {startDrawing} from "./automaton/canvas";
 
 ReactDom.render(
   <Provider store={store}>
@@ -14,31 +12,4 @@ ReactDom.render(
   document.getElementById("app")
 );
 
-const targetCanvas = document.getElementById("canvas") as HTMLCanvasElement;
-targetCanvas.width = document.body.clientWidth;
-targetCanvas.height = document.body.clientHeight;
-
-const CELL_SIZE = 10;
-const PATTERN_WIDTH = 60;
-const PATTERN_HEIGHT = 60;
-
-let cells = initialize(PATTERN_WIDTH, PATTERN_HEIGHT);
-
-const d = () => {
-  draw({
-    width: CELL_SIZE * PATTERN_WIDTH,
-    height: CELL_SIZE * PATTERN_HEIGHT,
-    cellSize: CELL_SIZE,
-    target: targetCanvas,
-    cells
-  });
-  cells = getNext(cells, strategies.DazzleStrategy);
-
-  requestAnimationFrame(d);
-};
-
-d();
-
-window.onclick = () => {
-  cells = initialize(PATTERN_WIDTH, PATTERN_HEIGHT);
-}
+startDrawing();
